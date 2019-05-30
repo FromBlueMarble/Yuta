a59673a5a261320535c176e20f823ddaa250804e
Yuta <frommyheart0405@gmail.com>
2019-05-29 23:59:57 -0700

    node.js myStudy Connect to MongoDB

---
+++ node.js/myStudy/mongo.js
@@ -0,0 +1,6 @@
+var MongoClient = recuire('mongodb').MongoClient,
+    settings = require('./settings');
+MongoClient.connect("mongodb://"+settings.host+"/"+settings.db,function(err, db) {
+    if(err) { return console.dir(err); }
+    console.log("connected to  db");
+});
--- node.js/myStudy/settings.js
+++ node.js/myStudy/settings.js
@@ -1,5 +1,6 @@
 exports.port = 1337;
 exports.host = '192.168.1.68';
+exports.db =  'nodedb';
 
 // 127.0.0.1
 // 192.168.1.68
