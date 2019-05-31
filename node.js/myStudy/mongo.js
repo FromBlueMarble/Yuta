95defd6affef51516b23737e7004b8458171a73e
Yuta <frommyheart0405@gmail.com>
2019-05-30 17:17:04 -0700

    node.js myStudy added

--- node.js/myStudy/mongo.js
+++ node.js/myStudy/mongo.js
@@ -1,7 +1,25 @@
+    class Users 
+    {
+        constructor(fname,lname){
+            this.firstName = fname;
+            this.lastName = lname;
+        }
+
+    }
+    var personA = new Users("Jhon", "Shara")
+    var personB = new Users("Mike", "llll")
+
 var MongoClient = require('mongodb').MongoClient,
     settings = require('./settings');
 MongoClient.connect("mongodb://"+settings.host+"/"+settings.db,function(err, db) {
     if(err) { return console.dir(err); }
     console.log("connected to db");
-    db.collection("")
+    console.log(db);
+
+
+    var x = db.db("users")
+    x.collection("customers").insertOne(personA, function(err, res){
+        if (err) throw err;
+        console.log("insertED" + personA)
+    })
 });
