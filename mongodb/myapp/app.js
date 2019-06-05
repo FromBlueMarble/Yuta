7d4b490551ea3dddbebc86c6f7d1631c6c801daf
Yuta <frommyheart0405@gmail.com>
2019-06-05 00:32:00 -0700

    ejs template

--- mongodb/myapp/app.js
+++ mongodb/myapp/app.js
@@ -1,19 +1,25 @@
 var express = require('express'),
     app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+    //middleware
     // app.use(app.router); Error: 'app.router' is deprecated!
-        app.use(express.static(__dirname + '/public'));
+    //Most middleware (like logger) is no longer bundled with Express and must be installed separately
+    // app.use(express.logger('dev'));
+app.use(express.static(__dirname + '/public'));
+
     //To the name add "?" Becomes optional
 
     // app.get('/about', function(req, res) {
     //     res.send('about this page!');
     // });
-    app.get('/hello.txt', function(req, res) {
-        res.sendfile(__dirname + '/public/hello.txt');
+    app.get('/', function(req, res) {
+        res.status('index', {title: 'title'});
     });
 
     app.listen(3000);
-    console.log("sever starting...");
+    console.log("sever starting!");
 
 
 
