27ae02c63071fd0869011793b902fd6bac31a0c0
Yuta <frommyheart0405@gmail.com>
2019-06-04 21:47:42 -0700

    mongodb myapp app.js added sendfile

--- mongodb/myapp/app.js
+++ mongodb/myapp/app.js
@@ -1,33 +1,29 @@
-// var express = require('express'),
-//     app = express();
+var express = require('express'),
+    app = express();
 
-//     // app.use(app.router); Error: 'app.router' is deprecated!
+    // app.use(app.router); Error: 'app.router' is deprecated!
 
-//     //To the name add "?" Becomes optional
-//     app.get('/users/:name?', function(req, res) {
-//         if(req.params.name) {
-//             res.send('hello, ' + req.params.name);
-//         }else {
-//             res.send('hello, nobody!');
-//         }
-//     });
-//     // app.get('/about', function(req, res) {
-//     //     res.send('about this page!');
-//     // });
-//     app.get('/items/:id([0-9]+)', function(req, res) {
-//         res.send('item no: ' + req.params.id);
-//     });
+    //To the name add "?" Becomes optional
 
-//     app.listen(3000);
-//     console.log("sever starting...");
+    // app.get('/about', function(req, res) {
+    //     res.send('about this page!');
+    // });
+    app.get('/hello.txt', function(req, res) {
+        res.sendfile(__dirname + '/public/hello.txt');
+    });
 
-var express = require('express');
-var app = express();
+    app.listen(3000);
+    console.log("sever starting...");
 
-app.get('/', function(req,res) {
-    res.send('Hello World!');
-});
 
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!')
-});
+
+// var express = require('express');
+// var app = express();
+
+// app.get('/', function(req,res) {
+//     res.send('Hello World!');
+// });
+
+// app.listen(3000, function() {
+//     console.log('Example app listening on port 3000!')
+// });
