d6ddbfb25166ca22c97c67f40ee832a81b481791
Yuta <frommyheart0405@gmail.com>
2019-06-05 19:09:39 -0700

    Display a list of articles

--- mongodb/blog/app.js
+++ mongodb/blog/app.js
@@ -23,12 +23,13 @@ app.use(express.methodOverride());
 
     // routing
     app.get('/', post.index);
-    app.get('/posts/new', post.news);
-    app.post('/posts/create', post.create);
-    app.get('/posts/:id', post.show);
-    app.get('/posts/:id/edit', post.edit);
-    app.put('/posts/:id', post.update);
-    app.delete('/posts/:id/', post.destroy);
+
+    // app.get('/posts/new', post.news);
+    // app.post('/posts/create', post.create);
+    // app.get('/posts/:id', post.show);
+    // app.get('/posts/:id/edit', post.edit);
+    // app.put('/posts/:id', post.update);
+    // app.delete('/posts/:id/', post.destroy);
 
 
     app.listen(3000);
