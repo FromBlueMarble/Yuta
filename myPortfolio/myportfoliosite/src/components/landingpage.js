8127c680de0b4f62b941e7a20e3fc9b11aa2466c
Yuta <frommyheart0405@gmail.com>
2019-07-11 03:11:24 -0700

    add

--- myPortfolio/myportfoliosite/src/components/landingpage.js
+++ myPortfolio/myportfoliosite/src/components/landingpage.js
@@ -1,30 +1,39 @@
-import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import React, { Component } from 'react'; import { Grid, Cell } from 'react-mdl'; class Landing extends Component { render() { return (
+    <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+            <Cell col={12}>
+                <img src="https://www.w3schools.com/w3images/avatar2.png" alt="avatar" className="avatar-img" />
+                <div className="banner-text">
+                    <h1>Front end developer</h1>
+                    <hr/>
+                    <p>HTML/CSS | Bootstrap | JavaScript | React | Node.js | Express | MongoDB | WordPress
+                        <br/>PhotoShop | Illustrator | XD | UI/UX</p>
+    
+                    <div className="social-links">
+    
+                        {/* LinkdIn */}
+                        <a href="http://google/com" rel="noopenr noreferrer" target="_blank">
+                            <i className="fa fa-linkedin-square" aria-hidden="true" />
+                        </a>
+    
+                        {/* Github */}
+                        <a href="https://github.com/FromBlueMarble" rel="noopenr noreferrer" target="_blank">
+                            <i className="fa fa-github-square" aria-hidden="true" />
+                        </a>
 
-class Landing extends Component {
-    render() {
-        return (
-            <div style={{width: '100%', margin: 'auto'}}>
-                <Grid className="Landing-grid">
-                    <Cell col={12}>
-                        <img 
-                            src="https://www.w3schools.com/w3images/avatar2.png"
-                            alt="avatar"
-                            className="avatar-img"
-                            />
-                            <div className="banner-text">
-                                <h1>Front end developer</h1>
-                                <hr/>
-                            <p>HTML/CSS | Bootstrap | JavaScript | React | Node.js | Express | MongoDB | WordPress
-                            <br/>
-                            PhotoShop | Illustrator | XD | UI/UX</p>
-
-                            </div>
-                    </Cell>
-                </Grid>
-            </div>
-        )
-    }
-}
-
-export default Landing;
+                        {/* FreecodeCamp */}
+                        <a href="https://github.com/FromBlueMarble" rel="noopenr noreferrer" target="_blank">
+                            <i className="fa fa-free-code-camp" aria-hidden="true" />
+                        </a>
+{/* 
+                        {/* other }
+                        <a href="https://github.com/FromBlueMarble" rel="noopenr noreferrer" target="_blank">
+                            <i className="fa fa-free-code-camp" aria-hidden="true" />
+                        </a> */}
+    
+                    </div>
+                </div>
+            </Cell>
+        </Grid>
+    </div>
+    ) } } export default Landing;
--- myPortfolio/myportfoliosite/src/components/project.js
+++ myPortfolio/myportfoliosite/src/components/project.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 };
+    }
+
+    toggleCategories() {
+
+    if(this.state.activeTab === 0){
+        return(
+            <div><h1>This is React</h1></div>
+        )
+    }
+
+}
+
+
     render() {
         return (
-            <div><h1>Projects Page</h1></div>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} repple>
+                    <Tab>React</Tab>
+                    <Tab>React</Tab>
+                    <Tab>MongoDB</Tab>
+                </Tabs>
+
+                <section className="projects-grid">
+                    {this.toggleCategories()}
+                </section>
+            </div>
         )
     }
 }
