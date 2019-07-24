e8e52750f40400db0183d2227251b01add239c7f
Yuta <frommyheart0405@gmail.com>
2019-07-23 21:26:39 -0700

    add

--- javascript/udemy/script.js
+++ javascript/udemy/script.js
@@ -1,8 +1,63 @@
-var firstName ='Jone';
-console.log(firstName);
+// Control flow
 
-var lastName = 'Smith';
-var age = 28;
+//var
+// var firstName ='Jone';
+// console.log(firstName);
 
-var fullAge = true;
-console.log(fullAge);
+// var lastName = 'Smith';
+// var age = 28;
+
+// var fullAge = true;
+// console.log(fullAge);
+
+//if
+// var name = "Billy";
+// if (name === "Billy") {
+//     alert("hi Billy!");
+// }
+
+// name = "Susy";
+// "Susy"
+// if (name === "Billy") {
+//     alert("hi Billy!");
+// }
+
+//else
+if (name === "Billy") {
+    alert("hi Billy!");
+}else {
+    alert("humm I don't know you");
+}
+undefined
+
+// "Susy"
+// name = "Billy"
+// "Billy"
+// if (name === "Billy") {
+//     alert("hi Billy!");
+// }else {
+//     alert("humm I don't know you");
+// }
+
+//else if
+if (name === "Billy") {
+    alert("hi Billy!");
+} else if (name === "Susy") {
+    alert("hi Susy!");
+} else {
+    alert("I don't know you");
+}
+
+name
+"Billy"
+name = "Susy"
+"Susy"
+if (name === "Billy") {
+    alert("hi Billy!");
+} else if (name === "Susy") {
+    alert("hi Susy!");
+} else {
+    alert("I don't know you");
+}
+
+if (name === "Billy" || name === "Ann")
