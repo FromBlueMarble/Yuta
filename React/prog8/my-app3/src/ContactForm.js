c3f86444144fea13b3c23c4c5695ea8428462b48
Yuta <frommyheart0405@gmail.com>
2019-10-01 14:45:18 -0700

    react prog8

--- React/prog8/my-app3/src/ContactForm.js
+++ React/prog8/my-app3/src/ContactForm.js
@@ -5,18 +5,66 @@ class ContactForm extends React.Component {
         super(props)
         this.state = {
             isSubmitted: false,
+            email:"",
+            hasEmailError:false,
+
+            content:'',
+            hasContentError:false,
+
         };
     }
+
+    handleEmailChange(event){
+        const inputValue = event.target.value;
+        const isEmpty = inputValue === '';
+
+
+        this.setState({
+            email:inputValue,
+            hasEmailError:isEmpty,
+        });
+    }
+
+    handleContentChange(event){
+        const inputValue = event.target.value;
+        const inEmpty = inputValue === '';
+        this.setState({
+            content:inputValue,
+            hasContentError:inEmpty,
+        });
+    }
+
+
 
     handleSubmit() {
         this.setState({isSubmitted: true});
     }
 
     render() {
+        let emailErrorText;
+
+        if(this.state.hasEmailError){
+            emailErrorText = (
+                <p className='contact-message-error'>
+                Fill out your email address!
+                </p>
+            )
+        }
+        let contentErrorText;
+
+        if(this.state.hasContentError){
+            contentErrorText = (
+                <p className='contact-message-error'>
+                お問い合わせ内容を入力してください
+                </p>
+            );
+        }
+
+
         let ContactForm;
         if (this.state.isSubmitted){
             ContactForm = ( <div className='contact-submit-message'>
-            送信完了
+            Complete
             </div>
             );
         }else {
@@ -24,9 +72,21 @@ class ContactForm extends React.Component {
 
                     <form onSubmit ={()=> {this.handleSubmit()}}>
                     <p>メールアドレス（必須）</p>
-                    <input />
+                    <input 
+                    value = {this.state.email}
+                    onChange = {(event) => {this.handleEmailChange(event)}}
+                    />
+
+                    {emailErrorText}
+
                     <p>お問い合わせ内容（必須）</p>
-                    <textarea />
+                    <textarea 
+                    value = {this.state.content}
+                    onChange = {(event)=> {this.handleContentChange(event)}}
+                    />
+                    {contentErrorText}
+
+
                     <input
                     type='submit'
                     value='送信'
